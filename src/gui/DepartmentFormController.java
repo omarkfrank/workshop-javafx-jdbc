f943cc78ed76f4a1089e163f00a318f5cb06a857
omarkfrank <omarkfrank@yahoo.com>
2024-06-04 15:52:15 -0300

    Update department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -89,7 +89,7 @@ public class DepartmentFormController implements Initializable {
 
 		ValidationException exception = new ValidationException("Validation error");
 
-		obj.setId(Utils.tryParseToInt(txtId.getId()));
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
 
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
@@ -132,7 +132,5 @@ public class DepartmentFormController implements Initializable {
 		if (fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
 		}
-
 	}
-
 }
